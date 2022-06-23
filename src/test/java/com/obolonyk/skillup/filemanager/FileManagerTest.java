cbea752e60c80601dd8c53f6f25cd321b8d63919
Clio18 <estclio09@gmail.com>
2022-06-23 13:26:28 +0300

    1. FileManager: code fixes concerning to warnings

--- src/test/java/com/obolonyk/skillup/filemanager/FileManagerTest.java
+++ src/test/java/com/obolonyk/skillup/filemanager/FileManagerTest.java
@@ -10,34 +10,26 @@ class FileManagerTest {
     @Test
     @DisplayName("Test GetListOfFiles If Path Is Null And Check Exception")
     void testGetListOfFilesIfPathIsNullAndCheckException (){
-        assertThrows(IllegalArgumentException.class,  () ->{
-            FileManager.getListOfFiles(null);
-        });
+        assertThrows(IllegalArgumentException.class,  () -> FileManager.getListOfFiles(null));
     }
 
     @Test
     @DisplayName("Test GetListOfFiles If Path Is Null And Check Exception Message")
     void testGetListOfFilesIfPathIsNullAndCheckExceptionMessage (){
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-            FileManager.getListOfFiles(null);
-        });
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> FileManager.getListOfFiles(null));
         assertEquals("Please provide a path", illegalArgumentException.getMessage());
     }
 
     @Test
     @DisplayName("Test GetListOfFiles If Path Incorrect And Check Exception")
     void testGetListOfFilesIfPathIsIncorrectAndCheckException (){
-        assertThrows(NullPointerException.class,  () ->{
-            FileManager.getListOfFiles("wrong/path");
-        });
+        assertThrows(NullPointerException.class,  () -> FileManager.getListOfFiles("wrong/path"));
     }
 
     @Test
     @DisplayName("Test GetListOfFiles If Path Incorrect And Check Exception Message")
     void testGetListOfFilesIfPathIsIncorrectAndCheckExceptionMessage (){
-        NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> {
-            FileManager.getListOfFiles("wrong/path");
-        });
+        NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> FileManager.getListOfFiles("wrong/path"));
         assertEquals("The files do not exist on provided path " + "wrong/path", nullPointerException.getMessage());
     }
 
