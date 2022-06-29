c355c84fc046a077dc4cd0ec85eb892f5521df08
Clio18 <estclio09@gmail.com>
2022-06-29 16:36:42 +0300

    Added more tests

--- src/test/java/com/obolonyk/skillup/fileanalyzer/FileAnalyzerITest.java
+++ src/test/java/com/obolonyk/skillup/fileanalyzer/FileAnalyzerITest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FileAnalyzerITest {
 
     String path = "text.txt";
+    String pathWithCyrylica = "text2.txt";
 
     @Test
     @DisplayName("Test GetTotalCount With UpperCase Word")
@@ -49,5 +50,45 @@ class FileAnalyzerITest {
         String word = "avaj";
         FileAnalyzer fileAnalyzer = new FileAnalyzer();
         assertEquals(0, fileAnalyzer.analyze(word, path).getCount());
+    }
+
+    @Test
+    @DisplayName("Test GetTotalCount With UpperCase Word Path With Cyrylica")
+    void testGetTotalCountWithUpperCaseWordPathWithCyrylica() throws IOException {
+        String word = "Java";
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+        assertEquals(8, fileAnalyzer.analyze(word, pathWithCyrylica).getCount());
+    }
+
+    @Test
+    @DisplayName("Test GetTotalCount With LowerCase Word Path With Cyrylica")
+    void testGetTotalCountWithLowerCaseWordPathWithCyrylica() throws IOException {
+        String word = "java";
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+        assertEquals(8, fileAnalyzer.analyze(word, pathWithCyrylica).getCount());
+    }
+
+    @Test
+    @DisplayName("Test GetTotalCount With LowerCase Word And Check Amount Of Sentences Path With Cyrylica")
+    void testGetTotalCountWithLowerCaseWordAndCheckAmountOfSentencesPathWithCyrylica() throws IOException {
+        String word = "java";
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+        assertEquals(5, fileAnalyzer.analyze(word, pathWithCyrylica).getSentences().size());
+    }
+
+    @Test
+    @DisplayName("Test GetTotalCount With LowerCase Not Existing Word And Check Amount Of Sentences Path With Cyrylica")
+    void testGetTotalCountWithLowerCaseNotExistingWordAndCheckAmountOfSentencesPathWithCyrylica() throws IOException {
+        String word = "avaj";
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+        assertEquals(0, fileAnalyzer.analyze(word, pathWithCyrylica).getSentences().size());
+    }
+
+    @Test
+    @DisplayName("Test GetTotalCount With LowerCase Not Existing Word And Check Count Path With Cyrylica")
+    void testGetTotalCountWithLowerCaseNotExistingWordAndCheckCountPathWithCyrylica() throws IOException {
+        String word = "avaj";
+        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+        assertEquals(0, fileAnalyzer.analyze(word, pathWithCyrylica).getCount());
     }
 }
