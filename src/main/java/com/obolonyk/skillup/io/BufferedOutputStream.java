d3d8c1067fb08f4f20d4ca046a9d04b28926c132
Clio18 <estclio09@gmail.com>
2022-07-03 23:12:59 +0300

    BufferedOutputStream was implemented, tests were added

--- src/main/java/com/obolonyk/skillup/io/BufferedOutputStream.java
+++ src/main/java/com/obolonyk/skillup/io/BufferedOutputStream.java
@@ -4,28 +4,56 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class BufferedOutputStream extends OutputStream {
+
+    private OutputStream target;
+    private byte[] buffer;
+    private static final int CAPACITY = 5;
+    private int position;
+
+
+    public BufferedOutputStream(OutputStream target) {
+        this.target = target;
+        buffer = new byte[CAPACITY];
+    }
+
+
     @Override
     public void write(int b) throws IOException {
-
+        if (position == CAPACITY) {
+            writeBuffer();
+        }
+        buffer[position++] = (byte) b;
     }
 
     @Override
-    public void write(byte[] b) throws IOException {
-        super.write(b);
+    public void write(byte[] bytes) throws IOException {
+        write(bytes, 0, bytes.length);
     }
 
     @Override
-    public void write(byte[] b, int off, int len) throws IOException {
-        super.write(b, off, len);
+    public void write(byte[] bytes, int off, int len) throws IOException {
+        System.arraycopy(bytes, off, buffer, position, len);
+        position = position + len;
     }
 
     @Override
     public void flush() throws IOException {
-        super.flush();
+        if (buffer == null) {
+            throw new IOException("Stream is closed");
+        }
+        writeBuffer();
+        target.flush();
     }
 
     @Override
     public void close() throws IOException {
-        super.close();
+        target.close();
+    }
+
+    private void writeBuffer() throws IOException {
+        if (position > 0) {
+            target.write(buffer, 0, position);
+            position = 0;
+        }
     }
 }
---
+++ src/test/java/com/obolonyk/skillup/io/BufferedOutputStreamTest.java
@@ -0,0 +1,53 @@
+package com.obolonyk.skillup.io;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BufferedOutputStreamTest {
+
+    @Test
+    @DisplayName("Test Write Array Of Bytes And Check Result Part")
+    void testWriteArrayOfBytesAndCheckResultPart() throws IOException {
+        OutputStream outputStream = new FileOutputStream("text3.txt");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        bufferedOutputStream.write('H');
+        bufferedOutputStream.write('e');
+        bufferedOutputStream.write('l');
+        bufferedOutputStream.write('l');
+        bufferedOutputStream.write('o');
+        bufferedOutputStream.flush();
+        FileInputStream fileInputStream = new FileInputStream("text3.txt");
+        assertEquals('H',fileInputStream.read());
+        assertEquals('e',fileInputStream.read());
+        assertEquals('l',fileInputStream.read());
+        assertEquals('l',fileInputStream.read());
+        assertEquals('o',fileInputStream.read());
+        assertEquals(-1,fileInputStream.read());
+        File file = new File("text3.txt");
+        file.delete();
+    }
+
+    @Test
+    @DisplayName("Test Write method with parameters")
+    void testWriteWithParam() throws IOException {
+        OutputStream outputStream = new FileOutputStream("text3.txt");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        bufferedOutputStream.write("Hello".getBytes());
+        bufferedOutputStream.flush();
+        FileInputStream fileInputStream = new FileInputStream("text3.txt");
+        assertEquals('H',fileInputStream.read());
+        assertEquals('e',fileInputStream.read());
+        assertEquals('l',fileInputStream.read());
+        assertEquals('l',fileInputStream.read());
+        assertEquals('o',fileInputStream.read());
+        assertEquals(-1,fileInputStream.read());
+        File file = new File("text3.txt");
+        file.delete();
+    }
+
+
+}
