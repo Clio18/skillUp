114d07adf1068f4116f9b878840aa6e0c1b8a98f
Clio18 <estclio09@gmail.com>
2022-07-21 20:02:54 +0300

    Serialization: small fixes

--- src/main/java/com/obolonyk/skillup/serialization/SerializationMessageDao.java
+++ src/main/java/com/obolonyk/skillup/serialization/SerializationMessageDao.java
@@ -5,28 +5,29 @@ import com.obolonyk.skillup.io.BufferedOutputStream;
 import com.obolonyk.skillup.io.ByteArrayInputStream;
 import com.obolonyk.skillup.io.ByteArrayOutputStream;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class SerializationMessageDao implements MessageDao {
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+    ByteArrayOutputStream byteArrayOutputStream;
+
 
     @Override
     public void save(Message message) throws IOException {
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream);
-        objectOutputStream.writeObject(message);
-        objectOutputStream.flush();
-        objectOutputStream.close();
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(bufferedOutputStream)) {
+            objectOutputStream.writeObject(message);
+            objectOutputStream.flush();
+        }
     }
 
     @Override
     public Message load() throws IOException, ClassNotFoundException {
         byte[] bytes = byteArrayOutputStream.toByteArray();
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(byteArrayInputStream);
-        ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream);
-        return (Message) objectInputStream.readObject();
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(byteArrayInputStream);
+             ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream)) {
+            return (Message) objectInputStream.readObject();
+        }
     }
 }
