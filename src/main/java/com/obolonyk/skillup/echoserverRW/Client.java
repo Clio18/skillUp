a610e23ae6ed045dd0d001f086abb2900b2c4174
Clio18 <estclio09@gmail.com>
2022-07-24 17:50:22 +0300

    EchoServer RW: make it works with console input

--- src/main/java/com/obolonyk/skillup/echoserverRW/Client.java
+++ src/main/java/com/obolonyk/skillup/echoserverRW/Client.java
@@ -2,6 +2,7 @@ package com.obolonyk.skillup.echoserverRW;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -10,15 +11,21 @@ public class Client {
     private static final String END_PROGRAM = "-1";
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(HOST, PORT);
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        String message = "New!";
-        writer.write(message);
-        writer.newLine();
-        writer.flush();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line = reader.readLine();
-        System.out.println(line);
+        try (Socket socket = new Socket(HOST, PORT);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String message = scanner.nextLine();
+                if (message.equals(END_PROGRAM)) {
+                    return;
+                }
+                writer.write(message);
+                writer.newLine();
+                writer.flush();
+                String line = reader.readLine();
+                System.out.println(line);
+            }
+        }
     }
 }
--- src/main/java/com/obolonyk/skillup/echoserverRW/Server.java
+++ src/main/java/com/obolonyk/skillup/echoserverRW/Server.java
@@ -8,16 +8,18 @@ public class Server {
     private static final int PORT = 3005;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket socket = serverSocket.accept();
+        try (ServerSocket serverSocket = new ServerSocket(PORT);
+             Socket socket = serverSocket.accept();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line = reader.readLine();
-        String resp = "Echo: " + line;
-
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        writer.write(resp);
-        writer.newLine();
-        writer.flush();
+            while (true) {
+                String line = reader.readLine();
+                String resp = "Echo: " + line;
+                writer.write(resp);
+                writer.newLine();
+                writer.flush();
+            }
+        }
     }
 }
